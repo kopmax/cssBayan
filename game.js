9340ab14e9110f752e41932fdba3f09ac62fedeb
kopmax <kopmax@bk.ru>
2023-03-24 16:27:17 +0300

    7

--- game.js
+++ game.js
@@ -2,10 +2,30 @@
 let heroImg = window.document.querySelector('.heroImg')
 // let rBtn = window.document.querySelector('.rightBtn')
 // let lBtn = window.document.querySelector('.leftBtn')
+let canvas = window.document.querySelector('#canvas')
+let fsBtn = window.document.querySelector('#fsBtn')
+
+fsBtn.onclick = () => {
+    console.log(window.document.fullscreenEnabled)
+    if (window.document.fullscreenElement) {
+        fsBtn.src = 'game/imgs/fullscreen.png'
+        window.document.exitFullscreen()
+    } else {
+        fsBtn.src = 'game/imgs/cancel.png'
+        canvas.requestFullscreen()
+    }
+}
+
 let imgBlock = window.document.querySelector('.hero-sprite-focus')
 let rightPostion = 0
+let direction = 'right'
 let imgBlockPosition = 0
 let timer = null
+const lifeCycle = () => {
+    timer = setInterval(() => {
+        standHandler()
+    }, 100)
+}
 let halfWidth = window.screen.width / 2
 
 
@@ -13,35 +33,80 @@ let halfWidth = window.screen.width / 2
 
 
 let onTouchStart = (event) => {
-    let x = event.screenX
-    console.log(halfWidth)
-    if (x > halfWidth) move = rightHandler
-    else move = leftHandler
+    clearInterval(timer)
+    let x = event.pageX
     timer = setInterval(() => {
-        move()
+       if (x > halfWidth) {
+        direction = 'right'
+        rightHandler()
+        } else { 
+            direction = 'left'
+            leftHandler() 
+        }
     }, 100)
 }
 
 let onTouchEnd = (event) => {
     clearInterval(timer)
+    lifeCycle()
 }
 
 const rightHandler = () => {
+    heroImg.style.transform = "scale(-1, 1)"
     rightPostion = rightPostion + 1
     imgBlockPosition = imgBlockPosition + 1
     if (rightPostion > 5) rightPostion = 0
     heroImg.style.left = `-${rightPostion*288}px`
+    heroImg.style.top = '-576px'
     imgBlock.style.left = `${imgBlockPosition*20}px`    
 }
 
 const leftHandler = () => {
-    heroImg.style.transform = 'scale(1, 1)'
+    heroImg.style.transform = "scale(1, 1)"
     rightPostion = rightPostion + 1
-    imgBlockPosition = imgBlockPosition + 1
+    imgBlockPosition = imgBlockPosition - 1
     if (rightPostion > 5) rightPostion = 0
     heroImg.style.left = `-${rightPostion*288}px`
+    heroImg.style.top = '-576px'
     imgBlock.style.left = `${imgBlockPosition*20}px`
+}
+
+const standHandler = () => {
+    heroImg.style.transform = "scale(-1, 1)"
+    
+    imgBlockPosition = imgBlockPosition + 1
+    if (rightPostion > 4) rightPostion = 1
+
+    switch (direction) {
+        case 'right' : {
+            heroImg.style.transform = "scale(-1, 1)"
+        rightPostion = rightPostion + 1
+        imgBlockPosition = imgBlockPosition + 1
+        if (rightPostion > 3) rightPostion = 0
+            break
+        }
+        case 'left' : {
+            heroImg.style.transform = "scale(1, 1)"
+        rightPostion = rightPostion + 1
+        imgBlockPosition = imgBlockPosition + 1
+        if (rightPostion > 5) rightPostion = 0
+            break
+        }
+        default: break;
+    }
+
+    rightPostion = rightPostion + 1
+    heroImg.style.left = `-${rightPostion*288}px`
+    heroImg.style.top = '-0px'
+    // imgBlock.style.left = `${imgBlockPosition*20}px` 
 }
 
 window.onmousedown = onTouchStart;
 window.onmouseup = onTouchEnd;
+
+
+const start = () => {
+    lifeCycle()
+}
+
+start()
