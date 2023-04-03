d66ef9d7dad854d0948a6a9c21b5d3ab6ced5336
kopmax <kopmax@bk.ru>
2023-04-03 16:31:12 +0300

    pre_end

---
+++ activeBox/app.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    let header = $('#header')
+    let intro = $('#intro')
+    let introH = intro.innerHeight()
+    let scrollPos = $(window).scrollTop()
+
+    $(window).on('scroll', function() {
+        scrollPos = $(this).scrollTop()
+
+        if(scrollPos > introH) {
+            header.addClass('fixed')
+        } else {
+            header.removeClass('fixed')
+        }
+
+        console.log(scrollPos)
+    })
+
+    console.log(scrollPos)
+
+    header.addClass('fixed')
+});
